1f4ef94a354d53b8a8af810066f8fafb3f0a0da6
aabubakar17 <abubakarabdihakim77@gmail.com>
2024-05-12 16:27:12 +0100

    NEW: Test Passed: Create Contact with Missing Information

--- src/main/java/com/addressbook/app/Contact.java
+++ src/main/java/com/addressbook/app/Contact.java
@@ -6,9 +6,16 @@ public class Contact {
     private  String email;
 
     public Contact(String name, String phoneNumber, String email) {
+        if (isEmpty(name) || isEmpty(phoneNumber) || isEmpty(email)) {
+            throw new IllegalArgumentException("All contact fields are required");
+        }
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.email = email;
+    }
+
+    private boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
     }
 
     public String getName() {
--- src/test/java/com/addressbook/app/ContactTest.java
+++ src/test/java/com/addressbook/app/ContactTest.java
@@ -5,16 +5,16 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ContactTest {
+    private final String name = "karry hane";
+    private final String phoneNumber = "07956809739";
+    private final  String email = "karry.hane@gmail.com";
 
     @Test
-    @DisplayName("Test adding contact with name, phone number, and email address")
-    void testAddContactWithNamePhoneNumberAndEmailAddress() {
-        // Arrange
-        String name = "karry hane";
-        String phoneNumber = "07956809739";
-        String email = "karry.hane@gmail.com";
+    @DisplayName("Test creating contact with name, phone number, and email address")
+    void testCreateContactWithNamePhoneNumberAndEmailAddress() {
 
         // Act
         Contact contact = new Contact(name, phoneNumber, email);
@@ -25,5 +25,41 @@ public class ContactTest {
                 () ->assertEquals(phoneNumber, contact.getPhoneNumber()),
                 () -> assertEquals(email, contact.getEmail()));
 
+    }
+
+    @Test
+    @DisplayName("Test creating contact with missing name")
+    void testCreateContactWithMissingName() {
+       String emptyName = "";
+        // Act
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Contact(emptyName, phoneNumber, email));
+
+        //Assert
+        assertEquals("All contact fields are required", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test creating contact with missing phone number")
+    void testCreateContactWithMissingPhoneNumber() {
+        String emptyPhoneNumber = "";
+        // Act
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Contact(name, emptyPhoneNumber, email));
+
+        //Assert
+        assertEquals("All contact fields are required", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test creating contact with missing email address")
+    void testCreateContactWithMissingEmailAddress() {
+        String emptyEmailAddress = "";
+        // Act
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Contact(name, phoneNumber, emptyEmailAddress));
+
+        //Assert
+        assertEquals("All contact fields are required", exception.getMessage());
     }
 }
